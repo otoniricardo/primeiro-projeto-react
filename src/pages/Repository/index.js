522cefb1c28754bd4452cfdcf150cb41b726c1d5
otoniricardo <otoniricardo@gmail.com>
2020-03-23 10:55:23 -0300

    Aula 11 - Carregando dados da api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [response, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: { state: 'open', per_page: 5 },
+      }),
+    ]);
+
+    this.setState({
+      loading: false,
+      repository: response.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <h1>Repository: {}</h1>;
+  }
 }
 // export default Repository;
